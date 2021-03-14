d74edde7cb2acdd3550977a37c8f5c6fe40d8431
eXpert <johkirwa17@gmail.com>
2021-03-14 15:57:11 +0300

    initial commit

--- app/src/main/java/com/example/mechtech/MainActivity.java
+++ app/src/main/java/com/example/mechtech/MainActivity.java
@@ -2,28 +2,31 @@ package com.example.mechtech;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_click;
+    private static int SPLASH_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        btn_click = findViewById(R.id.button_click);
-
-        btn_click.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
+            public void run() {
+                Intent homeIntent=new Intent(MainActivity.this,Dash.class);
+                startActivity(homeIntent);
+                finish();
             }
-        });
+        },SPLASH_TIME_OUT);
     }
 }
