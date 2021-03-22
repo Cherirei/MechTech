d511bb9eae1d70c8420c3225605208bbfed9d7db
eXpert <johkirwa17@gmail.com>
2021-03-22 13:56:58 +0300

    initial commit

--- app/src/main/java/com/example/mechtech/UserProfileFrament.java
+++ app/src/main/java/com/example/mechtech/UserProfileFrament.java
@@ -1,15 +1,22 @@
 package com.example.mechtech;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -17,10 +24,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class UserProfileFrament extends Fragment {
 
     private View view;
-    TextView name, username, email, phoneNo, username2;
+    TextView name, username, email, phoneNo, username2,resetPassword;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+    FirebaseUser user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,11 +41,13 @@ public class UserProfileFrament extends Fragment {
         username2 = view.findViewById(R.id.username2);
         email = view.findViewById(R.id.profile_email);
         phoneNo = view.findViewById(R.id.profile_phoneNo);
+        resetPassword=view.findViewById(R.id.resetPassword);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
 
         userId = fAuth.getCurrentUser().getUid();
+        user=fAuth.getCurrentUser();
 
         DocumentReference documentReference = fStore.collection("Users").document(userId);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -48,6 +58,51 @@ public class UserProfileFrament extends Fragment {
                 username.setText(documentSnapshot.getString("UserName"));
                 email.setText(documentSnapshot.getString("Email"));
                 phoneNo.setText(documentSnapshot.getString("PhoneNo"));
+            }
+        });
+
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetPassword = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter new Password > 6 characters long.");
+                passwordResetDialog.setView(resetPassword);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the mail and send the link
+                        String newPassword = resetPassword.getText().toString().trim();
+                        if (newPassword.isEmpty()){
+                            Toast.makeText(getContext(), "Field is Empty!!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(getContext(), "Password Successfully Changed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), "Password Reset Failed!!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Close the Dialog
+                    }
+                });
+                passwordResetDialog.create().show();
+
             }
         });
         return view;
